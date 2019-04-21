36be0d7fc14101a55aa22097e9398c272fda27fd
Patrick Conwell <p.conwell@vanderbilt.edu>
2019-04-21 09:44:32 -0500

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,18 +1,23 @@
 from time import sleep
 from datetime import datetime
-from picamera import PiCamera
+from picamera import PiCamera as camera
 
-camera = PiCamera(resolution=(1280, 720), framerate=30)
+cam = camera(resolution=(3280, 1845))
+
 # Set ISO to the desired value
-camera.iso = 100
+cam.iso = 50
+
+# 'crop' the image to the area you want
+# These settings don't seem to make sense in a logical way
+# so you will just have to play with the numbers until you
+# get what you want. Best I can tell, x + w needs to equal 1
+# and y + h needs to equal 1 or you will distort the image.
+#(x, y, h, w)
+cam.zoom = (0.0, 0.25, .75, 1.0)
+
 # Wait for the automatic gain control to settle
+# If you remove this, the image exposure will be messed up
 sleep(2)
-# Now fix the values
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
-# Finally, take several photos with the fixed settings
-#camera.capture_sequence(['image%s.jpg' % datetime.now().strftime("%Y%m%d_%H%M") for i in range(10)])
-camera.capture_sequence(['./shared/rpi-timelapse/%s.jpg' % datetime.now().strftime("%Y%m%d_%H%M") for i in range(10)])
+
+# capture the image and name it as a timestamp
+cam.capture('./shared/rpi-timelapse/%s.jpg' % datetime.now().strftime("%Y%m%d_%H%M"))
