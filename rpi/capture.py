e034ce05a4f6135e6ea5b83803cf88e18ad7d0f7
Patrick Conwell <patrick.conwell@gmail.com>
2021-04-15 15:29:53 -0500

    take full size pictures, then crop in post production

--- rpi/capture.py
+++ rpi/capture.py
@@ -1,7 +1,6 @@
 from time import sleep
 import datetime
-from picamera import PiCamera as camera
-#import pandas as pd
+from picamera import PiCamera
 import csv
 
 
@@ -23,24 +22,14 @@ for row in csv_file:
 if datetime.datetime.now().time() > sunrise and datetime.datetime.now().time() < sunset:
 
         #if true, take a picture
-        cam = camera(resolution=(3280, 1845))
+        camera = PiCamera()
 
-        # Set ISO to the desired value
-        #cam.iso = 50
-        cam.iso = 50
-
-        # 'crop' the image to the area you want.
-        # These settings don't make sense to me in a logical way so you will
-        # just have to play with the numbers until you get what you want.
-        # The below setting, for example, will crop the image to the lower right part of the image.
-        # In other words, the left and top portion of the image will be removed.
-        # Best I can tell, x + w needs to equal 1 and y + h needs to equal 1 or you will distort the image.
-        # zoom = (x, y, h, w)
-        cam.zoom = (0.0, 0.25, .75, 1.0)
+        # set resolution
+        camera.resolution=(3280, 1845)
 
         # Wait for the automatic gain control to settle
         # If you remove this, the image exposure will be messed up
-        sleep(2)
+        sleep(3)
 
         # capture the image and name it as a timestamp
-        cam.capture(f'/home/pi/images/{datetime.datetime.now().strftime("%Y%m%d_%H%M")}.jpg')
+        camera.capture(f'/home/pi/images/{datetime.datetime.now().strftime("%Y%m%d_%H%M")}.jpg')
